f1cedb8709fdffe2389f170fb9161b35347b23fe
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-29 11:26:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+ 
+//   const [movies, setMovies] = useState([]);
+
+//   function fetchMoviesHandler() {
+
+//     fetch('https://swapi.dev/api/films/').then(response=>{
+//       return response.json();
+//     }).then(data=>{
+//       const transformedMovies = data.results.map(movieData=>{
+//         return {
+//           id:movieData.episode_id,
+//           title:movieData.title,
+//           openingText:movieData.opening_crawl,
+//           releaseDate:movieData.release_date
+//         }
+//       });
+//        setMovies(transformedMovies);
+//     });
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <section>
+//         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+//       </section>
+//       <section>
+//         <MoviesList movies={movies} />
+//       </section>
+//     </React.Fragment>
+//   );
+// }
+
+const [movies, setMovies] = useState([]);
+
+const [ isLoading, setIsLoading] = useState(false);
+
+async function fetchMoviesHandler() {
+  try {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => ({
+      id: movieData.episode_id,
+      title: movieData.title,
+      openingText: movieData.opening_crawl,
+      releaseDate: movieData.release_date
+    }));
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+return (
+  <React.Fragment>
+    <section>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>
+     {!isLoading && <MoviesList movies={movies} />}
+     {isLoading && <p>Loading...</p>}
+    </section>
+  </React.Fragment>
+);
+
+}
+
+export default App;
